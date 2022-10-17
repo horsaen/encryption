144abd0a234d1c21227516c0642d89e7b5d6de2b
horsaen <atealltheglue@protonmail.com>
2022-10-16 23:25:37 -0500

    fixed directories

--- decrypt.java
+++ decrypt.java
@@ -1,10 +1,8 @@
-// v0.1
-
 import java.io.*;
 
-public class encrypt {
+public class decrypt {
     public static void main(String[] args) throws IOException {
-        File filepath = new File("Black.png");
+        File filepath = new File("output/output.hsn");
         FileInputStream fileinput = new FileInputStream(filepath);
 
         byte[] bytes = new byte[(int)filepath.length()];
@@ -14,13 +12,13 @@ public class encrypt {
         int arr[] = new int[bytes.length];
         byte[] bytes2 = new byte[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = bytes[i] + 23;
+            arr[i] = bytes[i] - 23;
             bytes2[i] = (byte)arr[i];
             // appended because annoying
             // System.out.println(arr[i]);
         }
 
-        FileOutputStream output = new FileOutputStream("output/output.hsn");
+        FileOutputStream output = new FileOutputStream("output/output2.png");
         output.write(bytes2);
         output.close();
     }
